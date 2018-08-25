7231dcd6ba6b3ca9012dcf998b884baf04980913
london804 <alexechaparro@gmail.com>
2018-08-25 16:23:06 -0400

    refactored app to use several modules

--- APM-Start /src/app/app.module.ts
+++ APM-Start /src/app/app.module.ts
@@ -1,46 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
-
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star.component';
-import { ProductDetailComponent } from './products/product-detail.component';
-import { WelcomeComponent} from './home/welcome.component';
-import { ProductDetailGuard } from './products/product-detail.guard';
 
+import { WelcomeComponent} from './home/welcome.component';
+import { ProductModule } from './products/product.module';
 
 
 @NgModule({
   declarations: [ // declarations I define
     AppComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    ProductDetailComponent,
     WelcomeComponent
   ],
   imports: [ // declarations/modules Angular defines and third party sources
     BrowserModule,
-    FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
       { path: 'welcome', component: WelcomeComponent },
-      { path: 'products/:id',
-        canActivate: [ ProductDetailGuard ],
-
-       component: ProductDetailComponent },
-      { path: 'products', component: ProductListComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
 
-    ])
+    ]),
+    ProductModule
   ],
   
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] // only necessary in the AppModule
 })
 export class AppModule { }
